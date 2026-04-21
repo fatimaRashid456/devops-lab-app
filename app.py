0457efe23e2c1d996525ca55180cbb81934f5491
Safwan Rashid <76547989+safwanrashid4@users.noreply.github.com>
2026-04-21 18:15:46 +0500

    jenkins

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD!"
+    return "Hello from Jenkins CI/CD Pipeline!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
