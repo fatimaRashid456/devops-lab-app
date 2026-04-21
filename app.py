236d94513d684653793508a2f547841913394587
Safwan Rashid <76547989+safwanrashid4@users.noreply.github.com>
2026-04-21 13:00:21 +0500

    Added Flask Hello DevOps World app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello DevOps Feature Update")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello DevOps World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
